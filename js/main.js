4f7d42353ac5c22d0eb8b829a1bbf4693827b13b
Sourav <souravsharma191919@gmail.com>
2022-01-18 17:50:25 +0530

    audio

---
+++ js/main.js
@@ -0,0 +1,91 @@
+
+//dhindhora
+let dhindhora = document.getElementById('dhindhora')
+function playdhindhora() {
+    dhindhora.play();
+  }
+  
+  function pausedhindhora() {
+    dhindhora.pause();
+  }
+
+//safar
+let safar = document.getElementById('safar')
+function playsafar() {
+    safar.play();
+  }
+  
+  function pausesafar() {
+    safar.pause();
+  }
+
+//bas mein
+let basmein = document.getElementById('basmein')
+function playbasmein() {
+    basmein.play();
+  }
+  
+  function pausebasmein() {
+    basmein.pause();
+  }
+
+//heer ranjha
+let heerranjha = document.getElementById('heerranjha')
+function playheerranjha() {
+    heerranjha.play();
+  }
+  
+  function pauseheerranjha() {
+    heerranjha.pause();
+  }
+
+
+
+
+let x = document.getElementById("myaudio")
+
+function playAudio() {
+  x.play();
+}
+
+function pauseAudio() {
+  x.pause();
+}
+
+// Mirage
+let a1 = document.getElementById("mirage");
+
+function playmirage() {
+  a1.play();
+}
+
+function pausemirage() {
+  a1.pause();
+}
+
+
+//worry
+let a2 = document.getElementById("worry");
+
+function playworry() {
+  a2.play();
+}
+
+function pauseworry() {
+  a2.pause();
+}
+
+
+//newskin
+let a3 = document.getElementById("newskin");
+
+function playnewskin() {
+  a3.play();
+}
+
+function pausenewskin() {
+  a3.pause();
+}
+
+
+
